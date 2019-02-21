fc69fa0bad6ee4c73774d0826e5373003f9eb108
Suprabho <suprabho13@gmail.com>
2019-02-21 12:28:28 +0530

    react

--- react_trial/public/index.js
+++ react_trial/public/index.js
@@ -16,9 +16,9 @@ var hist = createBrowserHistory();
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
-      <Route path="/landing-page" component={LandingPage} />
+      
       <Route path="/profile-page" component={ProfilePage} />
-      <Route path="/login-page" component={LoginPage} />
+      
       <Route path="/" component={Components} />
     </Switch>
   </Router>,
